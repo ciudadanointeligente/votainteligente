d3a8287a8db14d0ee76525edd46a1646eb57092a
Pedro Daire <pdaire@gmail.com>
2012-09-12 16:20:55 -0300

    permite valores blancos y nulos para los atributos de los objetos

--- municipales2012/models.py
+++ municipales2012/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Comuna(models.Model):
 	nombre =  models.CharField(max_length=255)
 	slug =  models.CharField(max_length=255)
-	main_embedded = models.CharField(max_length=512)
+	main_embedded = models.CharField(max_length=512, blank=True, null=True)
 
 	def __unicode__(self):
 		return self.nombre
@@ -14,9 +14,9 @@ class Comuna(models.Model):
 
 class Area(models.Model):
 	nombre = models.CharField(max_length=255)
-	clase_en_carrusel = models.CharField(max_length=255)
-	segunda_clase = models.CharField(max_length=255)
-	link_detalle = models.CharField(max_length=255)
+	clase_en_carrusel = models.CharField(max_length=255, blank=True, null=True)
+	segunda_clase = models.CharField(max_length=255, blank=True, null=True)
+	link_detalle = models.CharField(max_length=255, blank=True, null=True)
 
 	def __unicode__(self):
 		return self.nombre
@@ -24,7 +24,7 @@ class Area(models.Model):
 
 class Dato(models.Model):
 	nombre = models.CharField(max_length=255)
-	imagen = models.CharField(max_length=255)
+	imagen = models.CharField(max_length=255, blank=True, null=True)
 
 	def __unicode__(self):
 		return self.nombre
@@ -33,17 +33,17 @@ class Indice(models.Model):
 	comuna = models.ForeignKey(Comuna)
 	area = models.ForeignKey(Area)
 	dato = models.ForeignKey(Dato)
-	encabezado = models.CharField(max_length=255)
-	numero_1 = models.CharField(max_length=255)
-	texto_1 = models.CharField(max_length=255)
-	numero_2 = models.CharField(max_length=255)
-	texto_2 = models.CharField(max_length=255)
-	texto_pie_pagina_1 = models.CharField(max_length=255)
-	numero_pie_pagina_1 = models.CharField(max_length=255)
-	texto_pie_pagina_2 = models.CharField(max_length=255)
-	numero_pie_pagina_2 = models.CharField(max_length=255)
-	texto_pie_pagina_3 = models.CharField(max_length=255)
-	numero_pie_pagina_3 = models.CharField(max_length=255)
+	encabezado = models.CharField(max_length=255, blank=True, null=True)
+	numero_1 = models.CharField(max_length=255, blank=True, null=True)
+	texto_1 = models.CharField(max_length=255, blank=True, null=True)
+	numero_2 = models.CharField(max_length=255, blank=True, null=True)
+	texto_2 = models.CharField(max_length=255, blank=True, null=True)
+	texto_pie_pagina_1 = models.CharField(max_length=255, blank=True, null=True)
+	numero_pie_pagina_1 = models.CharField(max_length=255, blank=True, null=True)
+	texto_pie_pagina_2 = models.CharField(max_length=255, blank=True, null=True)
+	numero_pie_pagina_2 = models.CharField(max_length=255, blank=True, null=True)
+	texto_pie_pagina_3 = models.CharField(max_length=255, blank=True, null=True)
+	numero_pie_pagina_3 = models.CharField(max_length=255, blank=True, null=True)
 	en_carrusel = models.BooleanField(default=False)
 
 
