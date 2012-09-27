be804a01d4bac87392076231d6683653d9eb7d8e
Felipe Álvarez <luisfelipealvarez@gmail.com>
2012-09-27 19:35:39 -0300

    Fix #8

--- municipales2012/urls.py
+++ municipales2012/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from views import HomeTemplateView, ComunaOverview, ComunaIndices, MetodologiaView, QuienesSomosView
+from views import HomeTemplateView, ComunaOverview, ComunaIndices, MetodologiaView, QuienesSomosView, ReportaView
 from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
@@ -9,7 +9,7 @@ urlpatterns = patterns('',
 	#static pages
 	url(r'^metodologia/?$', MetodologiaView.as_view(), name="metodologia"),
 	url(r'^somos/?$', QuienesSomosView.as_view(), name="somos"),
-	url(r'^fiscaliza/?$', TemplateView.as_view(template_name="municipales2012/reporta.html"), name="reporta"),
+	url(r'^fiscaliza/?$', ReportaView.as_view(), name="reporta"),
 	
 
 	#pages depending on the comuna
