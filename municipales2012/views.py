f72aa7ce61e5c73c3cf932e6621670aca1371544
Marcel Augsburger <maugsburger@votainteligente.cl>
2012-10-05 11:17:44 -0300

    Fixed saving questions problem

--- municipales2012/views.py
+++ municipales2012/views.py
@@ -74,7 +74,7 @@ class ComunaPreguntales(CreateView):
 			conversaciones[nombre_emisor] = mensaje
 
 		context['conversaciones'] = conversaciones
-		context['candidatos'] = candidatos_comuna
+		context['candidatos'] = zip(candidatos_comuna, range(1, len(candidatos_comuna)+1))
 		context['titulo'] = "Preguntas a los Candidatos de " + comuna.nombre
 		
 		return context
