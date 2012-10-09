f9493b7f5620bcdb731fe6521e633b678e90f11b
Marcel Augsburger <maugsburger@votainteligente.cl>
2012-10-09 11:01:01 -0300

    WIP preguntales field error messages

--- municipales2012/forms.py
+++ municipales2012/forms.py
@@ -20,10 +20,13 @@ class PreguntaForm(forms.ModelForm):
     	candidatos = Candidato.objects.filter (comuna = comuna)
 
         #self.fields['candidato'].widget = forms.CheckboxSelectMultiple()
+	print self.fields['remitente'].error_messages
         self.fields['candidato'].queryset = candidatos
 	self.fields['candidato'].error_messages = {'required': 'Debes elegir al menos un candidato'}
     	self.fields['remitente'].error_messages = {'required': 'Debes identificarte de alguna forma'}
+    	self.fields['remitente'].error_messages = {'invalid': 'El nombre del remitente es demasiado largo'}
     	self.fields['texto_pregunta'].error_messages = {'required': 'Debes hacer una pregunta'}
+    	self.fields['texto_pregunta'].error_messages = {'invalid': 'La pregunta es demasiado larga'}
     	self.fields['captcha'].error_messages['required'] = 'Debemos asegurarnos que no seas un robot'
     	self.fields['captcha'].error_messages['captcha_invalid'] = 'El captcha que ingresaste no es válido'
     	self.fields['captcha'].error_messages['invalid'] = 'El captcha que ingresaste no es válido'
