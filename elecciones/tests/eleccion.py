afd3b9aa642fc00fc5bbcfab9c2f0eeccfe7828d
J. Pablo Pérez Trabucco <jpereztrabucco@gmail.com>
2013-02-28 11:40:42 -0300

    Refactor EleccionModelTestCase

--- elecciones/tests/eleccion.py
+++ elecciones/tests/eleccion.py
@@ -14,37 +14,31 @@ from django.template import Template, Context
 from urllib2 import quote
 
 class EleccionModelTestCase(TestCase):
+	def setUp(self):
+	    super(EleccionModelTestCase, self).setUp()
+	    self.eleccion, created = Eleccion.objects.get_or_create (nombre=u"La eleccion",
+	    													slug=u"la-eleccion",
+	    													candideitorg_api_key=u"api-key",
+	    													main_embedded=u"http://www.candideit.org/usuario-candideitorg/la-eleccion/embeded",
+	    													messaging_extra_app_url=u"http://napistejim.cz/address=nachod",
+	    													mapping_extra_app_url=u"http://vecino.ciudadanointeligente.org/around?latitude=-33.429042;longitude=-70.611278",
+	    													featured=True,
+	    													searchable=True
+	    													)
+
 	def test_create_eleccion(self):
-		eleccion, created = Eleccion.objects.get_or_create(nombre=u"La eleccion", 
-														slug=u"la-eleccion",
-														candideitorg_api_key=u"api-key",
-														main_embedded=u"http://www.candideit.org/lfalvarez/rayo-x-politico/embeded",
-														messaging_extra_app_url=u"http://napistejim.cz/address=nachod",
-														mapping_extra_app_url=u"http://vecino.ciudadanointeligente.org/around?latitude=-33.429042;longitude=-70.611278",
-														featured=True)
-		self.assertTrue(created)
-		self.assertEquals(eleccion.nombre, u"La eleccion")
-		self.assertEquals(eleccion.slug, u"la-eleccion")
-		self.assertEquals(eleccion.main_embedded, u"http://www.candideit.org/lfalvarez/rayo-x-politico/embeded")
-		self.assertEquals(eleccion.messaging_extra_app_url, u"http://napistejim.cz/address=nachod")
-		self.assertEquals(eleccion.mapping_extra_app_url, u"http://vecino.ciudadanointeligente.org/around?latitude=-33.429042;longitude=-70.611278")
-		self.assertTrue(eleccion.featured)
+		self.assertTrue(self.eleccion)
+		self.assertEquals(self.eleccion.nombre, u"La eleccion")
+		self.assertEquals(self.eleccion.slug, u"la-eleccion")
+		self.assertEquals(self.eleccion.main_embedded, u"http://www.candideit.org/usuario-candideitorg/la-eleccion/embeded")
+		self.assertEquals(self.eleccion.messaging_extra_app_url, u"http://napistejim.cz/address=nachod")
+		self.assertEquals(self.eleccion.mapping_extra_app_url, u"http://vecino.ciudadanointeligente.org/around?latitude=-33.429042;longitude=-70.611278")
+		self.assertTrue(self.eleccion.featured)
 
 	def test_eleccion_unicode(self):
-		eleccion = Eleccion.objects.create(nombre=u"La eleccion", slug=u"la-eleccion")
-
-		self.assertEquals(eleccion.__unicode__(), eleccion.nombre)
+		self.assertEquals(self.eleccion.__unicode__(), self.eleccion.nombre)
 
 	def test_parsing_user_slug_from_candideitorg(self):
-		eleccion, created = Eleccion.objects.get_or_create (nombre=u"La eleccion",
-															slug=u"la-eleccion",
-															candideitorg_api_key=u"api-key",
-															main_embedded=u"http://candideit.org/usuario-candideitorg/la-eleccion/embeded",
-															messaging_extra_app_url=u"http://napistejim.cz/address=nachod",
-															mapping_extra_app_url=u"http://vecino.ciudadanointeligente.org/around?latitude=-33.429042;longitude=-70.611278",
-															featured=False,
-															searchable=True
-															)
-		self.assertEqual(eleccion.candideitorg_username(), u"usuario-candideitorg")
-		self.assertEqual(eleccion.candideitorg_election_slug(), u"la-eleccion")
-		self.assertEqual(eleccion.candideitorg_api_key, u"api-key")
+		self.assertEqual(self.eleccion.candideitorg_username(), u"usuario-candideitorg")
+		self.assertEqual(self.eleccion.candideitorg_election_slug(), u"la-eleccion")
+		self.assertEqual(self.eleccion.candideitorg_api_key, u"api-key")
